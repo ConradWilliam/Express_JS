519de70d45787677e5a965306f7ca23d88c108fa
ConradWilliam <92802571+ConradWilliam@users.noreply.github.com>
2023-02-28 12:11:48 +0300

    Add files via upload

---
+++ startingServer.js
@@ -0,0 +1,12 @@
+// Import the express library here
+const express = require('express');
+// Instantiate the app here
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+// Invoke the app's `.listen()` method below:
+// const PORT = 4001;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+})
