aa69affc8bb4a29a11395b00bef488d298004674
ConradWilliam <mcwilliam131@gmail.com>
2023-05-18 11:23:21 +0300

    Changes

--- middleware/app.js
+++ middleware/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const errorHandler = require('errorhandler');
 
+app.use(errorHandler());
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 4001;
@@ -24,29 +27,15 @@ const jellybeanBag = {
   }
 };
 
-const bodyParser = (req, res, next) => {
-  let queryData = '';
-  req.on('data', (data) => {
-    data = data.toString();
-    queryData += data;
-  });
-  req.on('end', () => {
-    if (queryData) {
-      req.body = JSON.parse(queryData);
-    }
-    next();
-  });
-};
+// Body-parsing Middleware
+app.use(bodyParser.json());
 
 // Logging Middleware
-app.use(morgan('tiny'),(req, res, next) => {
-  next();
-});
+app.use(morgan('dev'));
 
-app.use('/beans/:beanName',morgan('tiny'), (req, res, next) => {
+app.use('/beans/:beanName', (req, res, next) => {
   const beanName = req.params.beanName;
   if (!jellybeanBag[beanName]) {
-
     return res.status(404).send('Bean with that name does not exist');
   }
   req.bean = jellybeanBag[beanName];
@@ -56,36 +45,32 @@ app.use('/beans/:beanName',morgan('tiny'), (req, res, next) => {
 
 app.get('/beans/', (req, res, next) => {
   res.send(jellybeanBag);
-;
 });
 
-app.post('/beans/', bodyParser, (req, res, next) => {
+app.post('/beans/', (req, res, next) => {
   const body = req.body;
   const beanName = body.name;
   if (jellybeanBag[beanName] || jellybeanBag[beanName] === 0) {
-    return res.status(400).send('Bag with that name already exists!');
+    return res.status(400).send('Bean with that name already exists!');
   }
   const numberOfBeans = Number(body.number) || 0;
   jellybeanBag[beanName] = {
     number: numberOfBeans
   };
   res.send(jellybeanBag[beanName]);
-
 });
 
 app.get('/beans/:beanName', (req, res, next) => {
   res.send(req.bean);
-
 });
 
-app.post('/beans/:beanName/add', bodyParser, (req, res, next) => {
+app.post('/beans/:beanName/add', (req, res, next) => {
   const numberOfBeans = Number(req.body.number) || 0;
   req.bean.number += numberOfBeans;
   res.send(req.bean);
-
 });
 
-app.post('/beans/:beanName/remove', bodyParser, (req, res, next) => {
+app.post('/beans/:beanName/remove', (req, res, next) => {
   const numberOfBeans = Number(req.body.number) || 0;
   if (req.bean.number < numberOfBeans) {
     return res.status(400).send('Not enough beans in the jar to remove!');
@@ -100,12 +85,16 @@ app.delete('/beans/:beanName', (req, res, next) => {
   res.status(204).send();
 });
 
-app.put('/beans/:beanName/name', bodyParser, (req, res, next) => {
+app.put('/beans/:beanName/name', (req, res, next) => {
   const beanName = req.beanName;
   const newName = req.body.name;
   jellybeanBag[newName] = req.bean;
   jellybeanBag[beanName] = null;
   res.send(jellybeanBag[newName]);
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
 });
 
 app.listen(PORT, () => {
